045bc5a901cc05c7e3374ebaecdd035cb9bc5679
François Leurent <fleurent@ivsweb.com>
2015-09-11 22:41:02 +0200

    Initial work

---
+++ README.md
@@ -0,0 +1,22 @@
+# Installation
+```
+npm install expect-dom.js
+```
+
+# Usage
+
+
+```
+var expect = require('expect.js');
+
+  //extend expect with DOM related assertions
+require('expect-dom.js')(expect);
+
+```
+
+# Api
+
+* expect($(somelement)).to.be.visible();
+
+
+
---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict";
+var isVisible = require('udom/isVisible');
+
+
+function DOM(obj) {
+  if (obj && obj.tagName)
+    return obj;
+  if (obj && obj.jquery)
+    return obj.get(0);
+
+  throw "Do you provide a DOM or jQuery object?";
+}
+
+
+var assertions = {
+  visible = function(){
+      assert(this, isVisible(DOM(this.obj)),
+        "Expected element to be visible",
+        "Expected element to not be visible");
+  },
+};
+
+
+
+function assert(self, truth, msg, error) {
+  self.assert(truth,
+    function () { return msg; },
+    function () { return error; });
+}
+
+
+
+module.exports = function(expect){
+
+  for(var assertion in assertions)
+    expect.Assertion.prototype[assertion] = assertions[assertion];
+
+}
