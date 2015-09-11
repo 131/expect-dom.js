9b397603e3c4501f08ef9fc74d6b4e85d20254c6
François Leurent <fleurent@ivsweb.com>
2015-09-12 00:32:11 +0200

    Drop jquery, back to udom

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 "use strict";
 
-var $ = require('jquery').noConflict();
+var isVisible = require('udom/isVisible');
+
+
+function DOM(obj) {
+  console.log("testing ", obj, document.querySelector(obj).tagName);
+  if(typeof obj == "string")
+    return document.querySelector(obj);
+  if (obj && obj.tagName)
+    return obj;
+  if (obj && obj.jquery)
+    return obj.get(0);
+
+  throw "Do you provide a DOM or jQuery object?";
+}
 
 
 var assertions = {
   visible : function(){
-      assert(this, $(this.obj).is(':visible') ,
+      assert(this, isVisible(DOM(this.obj)) ,
         "Expected element to be visible",
         "Expected element to not be visible");
   },
